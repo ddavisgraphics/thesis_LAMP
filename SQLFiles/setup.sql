adb1f96781fe422a5f5db2e0e4a26dbb8e7bf400
ddavisgraphics <ddavisgraphics@gmail.com>
2015-11-18 19:19:01 -0500

    already done

--- SQLFiles/setup.sql
+++ SQLFiles/setup.sql
@@ -1,5 +1,3 @@
-CREATE USER 'username'@'localhost' IDENTIFIED BY 'password';
-
 DROP DATABASE IF EXISTS `timeTracker`;
 CREATE DATABASE IF NOT EXISTS `timeTracker`;
 GRANT ALL PRIVILEGES ON `timeTracker`.* TO 'username'@'localhost';
