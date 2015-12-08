5c6cad2d99a342af0e05a11d5490dbf6c0749252
ddavisgraphics <ddavisgraphics@gmail.com>
2015-12-08 07:51:45 -0500

    includes added and better naming on router

--- src/index.php
+++ src/index.php
@@ -1,14 +1,27 @@
 <?php
     require_once "includes/engine.php";
-    require_once "includes/models/models.php";
+    require_once "includes/models/index.php";
+    require_once "includes/controller/index.php";
 
     $router   = router::getInstance();
     $router->defineRoute("/", 'displayHome');
     $router->defineRoute("/{model}", 'displayRoute');
-    $router->defineRoute("/{model}/{item}", 'displayRoute');
-    $router->defineRoute("/{model}/{item}/{test}", 'displayRoute');
+    $router->defineRoute("/{model}/{action}", 'displayRoute');
+    $router->defineRoute("/{model}/{action}/{item}", 'displayRoute');
     $router->route();
 
-    print "Testing";
+    templates::display('header');
+?>
 
+<section>
+    <header>
+        <h1>{local var="pageHeader"}</h1>
+    </header>
+
+    {local var="content"}
+</section>
+
+
+<?php
+    templates::display('footer');
 ?>
