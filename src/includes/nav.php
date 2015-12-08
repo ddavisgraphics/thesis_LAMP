adc51fd0c926a9da62bcc3565c0919448096f6c0
ddavisgraphics <ddavisgraphics@gmail.com>
2015-12-08 07:49:57 -0500

    added nav

---
+++ src/includes/nav.php
@@ -0,0 +1,12 @@
+<?php
+
+?>
+
+<nav>
+    <ul>
+        <li><a href="/"> Home </a></li>
+        <li><a href="/customers"> Customers </a></li>
+        <li><a href="/projects"> Projects </a></li>
+        <li><a href="/timeTracking"> Time Tracking </a></li>
+    </ul>
+</nav>
