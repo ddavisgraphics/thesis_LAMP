284bdd331d7cc4f5ff6bd128ce54e4cecc32675a
ddavisgraphics <ddavisgraphics@gmail.com>
2015-12-08 07:51:16 -0500

    controller callback functions

--- src/includes/controller/controllers.php
+++ src/includes/controller/controllers.php
@@ -1,20 +1,29 @@
 <?php
-  //  class Controller {
-  //   public function home() {
-  //     recurseInsert('includes/views/pages/home.php', 'php');
-  //   }
-
-  //   public function error() {
-  //     recurseInsert('includes/views/pages/error.php', 'php');
-  //   }
-  // }
 
   function displayRoute($url, $vars){
+    $model  = $vars['model'];
+    $action = $vars['action'];
+    $item   = $vars['item'];
 
+    $expectedModels = array(
+      'customers', 'projects', 'timeTracker'
+    );
+
+    if(in_array($model, $expectedModels)){
+      print "Yay lets do something";
+    } else {
+      print "404";
+    }
+
+    $pageHeader =  $model;
   }
 
   function displayHome($url, $vars){
-    print "home page";
+    $localvars  = localvars::getInstance();
+
+    $view = new View('Home', array());
+    $html = $view->render();
+    $localvars->set('content', $html);
   }
 
 ?>
