1a9d6da32d45ccbf8f0ac64a24a949f96770ba86
ddavisgraphics <ddavisgraphics@gmail.com>
2015-12-08 20:29:45 -0500

    controller functions

--- src/includes/controller/controllers.php
+++ src/includes/controller/controllers.php
@@ -1,22 +1,39 @@
 <?php
+    function displayRoute($url, $vars){
+        // new instance of local variables
+        $localvars  = localvars::getInstance();
 
-  function displayRoute($url, $vars){
-    $model  = $vars['model'];
-    $action = $vars['action'];
-    $item   = $vars['item'];
+        // declar url vars
+        $model  = $vars['model'];
+        $action = isset($vars['action']) ? $vars['action'] : null;
+        $item   = isset($vars['item'])   ? $vars['item']   : null;
 
-    $expectedModels = array(
-      'customers', 'projects', 'timeTracker'
-    );
+        // expected pages
+        $expectedModels = array(
+            'customers', 'projects', 'timeTracker'
+        );
 
-    if(in_array($model, $expectedModels)){
-      print "Yay lets do something";
-    } else {
-      print "404";
+        if(in_array($model, $expectedModels)){
+             $pageVariables = array(
+                'model'  => ucfirst($model),
+                'action' => $action,
+                'item'   => $item
+            );
+
+            $view = new View($model, $pageVariables);
+            $html = $view->render();
+            $localvars->set('content', $html);
+        }
+        else {
+            $pageVariables = array(
+                'model' => ucfirst($model)
+            );
+
+            $view = new View('Error', $pageVariables);
+            $html = $view->render();
+            $localvars->set('content', $html);
+        }
     }
-
-    $pageHeader =  $model;
-  }
 
   function displayHome($url, $vars){
     $localvars  = localvars::getInstance();
