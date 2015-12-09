2f1f2c2c71eab51ba20410f9606ef8a96b3a6ada
ddavisgraphics <ddavisgraphics@gmail.com>
2015-12-08 20:28:47 -0500

    test error page

--- src/includes/views/error.php
+++ src/includes/views/error.php
@@ -1,2 +1,3 @@
-<h2> Error </h2>
+<h2>404 Error</h2>
+<p> Page Name - <?php print $this->data['model']; ?> </p>
 <p> The Page you requested failed.  Maybe the wrong name, or maybe the page doesn't exsist. </p>
