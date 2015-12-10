b4ccf78a5463f5d47a001105744b64c9cc490ed6
ddavisgraphics <ddavisgraphics@gmail.com>
2015-12-09 22:09:03 -0500

    updated error views

--- src/includes/views/error.php
+++ src/includes/views/error.php
@@ -1,3 +1,14 @@
+<?php
+    require_once "includes/engine.php";
+    require_once "includes/models/index.php";
+    require_once "includes/controller/index.php";
+
+    // Stuff to Render
+    $today = date('d-m-y');
+    $localvars  = localvars::getInstance();
+    $localvars->set('date', $today);
+    $localvars->set('pageName', ucfirst($this->data['model']));
+?>
 <h2>404 Error</h2>
-<p> Page Name - <?php print $this->data['model']; ?> </p>
+<p> <strong>{local var="pageName"}</strong>  - {local var="date"}</p>
 <p> The Page you requested failed.  Maybe the wrong name, or maybe the page doesn't exsist. </p>
