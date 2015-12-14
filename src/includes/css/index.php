96d2a941b72e291b0b83c8a5ee1ec7cfed422f7d
ddavisgraphics <ddavisgraphics@gmail.com>
2015-12-14 18:30:00 -0500

    Css for Router

---
+++ src/includes/css/index.php
@@ -0,0 +1,6 @@
+<?php
+/*
+This file is only here to cover a minor bug in the EngineAPI router module which requires an index file at each
+level of the directory tree in order to correctly route the request to the correct location
+*/
+?>
