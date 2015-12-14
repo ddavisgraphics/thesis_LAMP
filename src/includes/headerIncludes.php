24c8d8c0d114b0038f72dddfb9ada7fadec09337
ddavisgraphics <ddavisgraphics@gmail.com>
2015-12-14 18:30:51 -0500

    theme

--- src/includes/headerIncludes.php
+++ src/includes/headerIncludes.php
@@ -2,4 +2,8 @@
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 
 <!-- Optional theme -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+<link rel="stylesheet" href="http://bootswatch.com/flatly/bootstrap.min.css" />
+
+<!-- Custom CSS -->
+<link rel="stylesheet" type="text/css" href="/includes/css/main.css" />
+
