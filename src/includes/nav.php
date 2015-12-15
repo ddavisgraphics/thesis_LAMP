e5b84c4b9ce43c1b0e1e0f7a18e696056287f843
ddavisgraphics <ddavisgraphics@gmail.com>
2015-12-14 20:15:19 -0500

    changed markup on nav

--- src/includes/nav.php
+++ src/includes/nav.php
@@ -1,12 +1,27 @@
 <?php
 
 ?>
-
-<nav>
-    <ul>
+    <div class="navi collapse navbar-collapse">
+      <ul class="nav nav-pills pull-right">
         <li><a href="/"> Home </a></li>
-        <li><a href="/customers"> Customers </a></li>
-        <li><a href="/projects"> Projects </a></li>
-        <li><a href="/timeTracking"> Time Tracking </a></li>
-    </ul>
-</nav>
+
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"> Customers <span class="caret"></span></a>
+          <ul class="dropdown-menu" role="menu">
+            <li><a href="/customers"> Manage Customers </a></li>
+            <li><a href="/customers/add"> Add Customers </a></li>
+          </ul>
+        </li>
+
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"> Projects <span class="caret"></span></a>
+          <ul class="dropdown-menu" role="menu">
+            <li><a href="/projects"> Manage Projects </a></li>
+            <li><a href="/projects/add"> Add Projects </a></li>
+          </ul>
+        </li>
+
+        <li><a href="/timeTracker"> Track Time </a></li>
+
+      </ul>
+    </div>
