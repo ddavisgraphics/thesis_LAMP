c6e40a2a6696613d5285c5878dd188eda7f23c42
David J. Davis <djdavis@mail.wvu.edu>
2015-12-20 14:17:06 -0500

    better code

--- src/includes/views/customers.php
+++ src/includes/views/customers.php
@@ -1,7 +1,5 @@
 <?php
-    require_once "includes/engine.php";
-    require_once "includes/models/index.php";
-    require_once "includes/controller/index.php";
+   // require our functions
     require_once "includes/functions/index.php";
 
     // instantiate classes
